c772e86430a18b27ed7340215ac703ddb6969385
Edwin <qkr990509@naver.com>
2022-07-20 23:45:16 +0900

    0720

--- exam/01/repeat_alpha/repeat_alpha.c
+++ exam/01/repeat_alpha/repeat_alpha.c
@@ -1,21 +1,34 @@
 #include <unistd.h>
 
-void ()
-{
-}
-
 int	main(int argc, char **argv)
 {
-	if(argc != 2 || *argv[1] == "")
+	if(argc != 2)
 		return 0;
 	
 	while(*argv[1])
 	{
-		if(*argv[1] >= 'a' && *argv[1] <= 'z')
+		char ch = *argv[1];
+		if((*argv[1] >= 'a' && *argv[1] <= 'z')
+			|| (*argv[1] >= 'A' && *argv[1] <= 'Z'))
 		{
+			int count = 0;
+			if(*argv[1] >= 'a' && *argv[1] <= 'z')
+				count = *argv[1] - 96;
+			if(*argv[1] >= 'A' && *argv[1] <= 'Z')
+				count = *argv[1] - 64;
 
+			while(count != 0)
+			{
+				write(1,&ch,1);
+				count--;
+			}
+		}
+		else
+		{
+			write(1,&ch,1);
 		}
 		argv[1]++;
 	}
+	write(1,"\n",1);
 	return 0;
 }
